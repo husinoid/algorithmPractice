5724e9ec6f39837335f85e952cbd3f447a054eb8
husinoid <42468629+husinoid@users.noreply.github.com>
2019-04-10 01:17:08 +0900

    Add files via upload

---
+++ stamp.cpp
@@ -0,0 +1,23 @@
+//주어진 숫자만큼 # 을 출력해보세요.
+//주어질 숫자는 100,000 이하다.
+
+
+
+#include <stdio.h>
+
+int main () {
+
+    int n;
+    char stamp = '#';
+    scanf("%d", &n);
+
+    if ( n <= 100000){
+        for (int i = 0; i < n; ++i) {
+            printf("%c", stamp);
+        }
+    }else {
+        printf("Out of number");
+    }
+
+    return 0;
+}
