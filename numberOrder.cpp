a25fd368f19635089f3733aabc0c7df45c1d8650
husinoid <42468629+husinoid@users.noreply.github.com>
2019-04-10 19:59:19 +0900

    Update numberOrder.cpp

--- numberOrder.cpp
+++ numberOrder.cpp
@@ -1,16 +1,5 @@
-/*주어진 N 길이의 숫자열을 오름차순으로 정렬하여 출력하라.
-[제약 사항]
-N 은 5 이상 50 이하이다.
-[입력]
-가장 첫 줄에는 테스트 케이스의 개수 T가 주어지고, 그 아래로 각 테스트 케이스가 주어진다.
-각 테스트 케이스의 첫 번째 줄에 N 이 주어지고, 다음 줄에 N 개의 숫자가 주어진다.
-[출력]
-출력의 각 줄은 '#t'로 시작하고, 공백을 한 칸 둔 다음 정답을 출력한다.
-(t는 테스트 케이스의 번호를 의미하며 1부터 시작한다.)
-https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PrmyKAWEDFAUq&categoryId=AV5PrmyKAWEDFAUq&categoryType=CODE
- */
-
-/*
+//site from 'SW Expert Academy'
+//link URL: https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PrmyKAWEDFAUq&categoryId=AV5PrmyKAWEDFAUq&categoryType=CODE
 
 #include <stdio.h>
 
@@ -50,4 +39,4 @@ int main () {
     return 0;
 }
 
- */
+ 
