d93591a6bdf0a22d520eea3af912134b9819379b
husinoid <42468629+husinoid@users.noreply.github.com>
2019-04-13 22:27:37 +0900

    Update chickenDeliver.cpp

--- chickenDeliver.cpp
+++ chickenDeliver.cpp
@@ -1,15 +1,9 @@
-/*입력
-첫째 줄에 N(2 ≤ N ≤ 50)과 M(1 ≤ M ≤ 13)이 주어진다.
-둘째 줄부터 N개의 줄에는 도시의 정보가 주어진다.
-도시의 정보는 0, 1, 2로 이루어져 있고, 0은 빈 칸, 1은 집, 2는 치킨집을 의미한다. 집의 개수는 2N개를 넘지 않으며, 적어도 1개는 존재한다. 치킨집의 개수는 M보다 크거나 같고, 13보다 작거나 같다.
-출력
-첫째 줄에 폐업시키지 않을 치킨집을 최대 M개를 골랐을 때, 도시의 치킨 거리의 최솟값을 출력한다.
-
-https://www.acmicpc.net/problem/15686
+/*
+//site from 'BaekJoon'
+//link URL: https://www.acmicpc.net/problem/15686
+//Lecture URL: https://www.youtube.com/watch?v=EWzqP8KoCXA&list=PLdHw4xVmS1psZInw07xdgZmnQcIrXru8J&index=18
  */
 
-
-/*
 #include <stdio.h>
 #include <algorithm>
 #include <vector>
@@ -73,4 +67,3 @@ int main () {
 
     return 0;
 }
-*/
